283bf2a58d448eb589a87923d4c8e690f7850159
kctiraj44 <kctiraj44@gmail.com>
2021-10-09 11:14:21 -0500

    Added all trhe file-Part1

---
+++ src/main/java/com/tiraj/app/Git_Demo_Part1/App.java
@@ -0,0 +1,19 @@
+package com.tiraj.app.Git_Demo_Part1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	
+	public void addData(String data) {
+		System.out.println("data -"+data);
+	}
+	
+	
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
